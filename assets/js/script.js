1401f8ae8f9deb2e026ccec5a67dfe7fa6d18ba0
Jeff Zenko <jeff.zenko@gmail.com>
2023-03-11 00:29:34 -0500

    basic styling, realistic timer simulation. Still need to connect JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,8 @@ var timeDisplay = document.querySelector("time-display");
 //time pieces
 var second = 1000;
 var minute = second * 60;
-var hour = minute * 60
+var hour = minute * 60;
+
+var defaultWorkTime = 20 * minute;
+var defaultShortBreakTime = 5 * minute
+var longBreakTime = 10 * minute;
